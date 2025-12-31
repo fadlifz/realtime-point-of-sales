b4d76ddaa28896f74b66ecb644cb24736ca0e07d
fadlifz <fadlifaturzaman@gmail.com>
2025-12-31 08:17:54 +0700

    fixing cookieStore issue

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,6 +1,7 @@
 import { environment } from "@/configs/environment";
 import { createServerClient } from "@supabase/ssr";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -10,13 +11,17 @@ export async function updateSession(request: NextRequest) {
   const { SUPABASE_URL, SUPABASE_ANON_KEY, SUPABASE_SERVICE_ROLE_KEY } =
     environment;
 
+  const cookieStore = await cookies();
+
   const supabase = createServerClient(SUPABASE_URL!, SUPABASE_ANON_KEY!, {
     cookies: {
       getAll() {
         return request.cookies.getAll();
       },
       setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value }) => cookieStore.set(name, value));
+        cookiesToSet.forEach(({ name, value, options }) =>
+          cookieStore.set(name, value, options)
+        );
         supabaseResponse = NextResponse.next({ request });
         cookiesToSet.forEach(({ name, value, options }) =>
           supabaseResponse.cookies.set(name, value, options)
