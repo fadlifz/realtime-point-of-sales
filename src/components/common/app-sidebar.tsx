9968166f5589a11e22584efd5a2de10803f44775
fadlifz <fadlifaturzaman@gmail.com>
2026-02-05 13:02:18 +0700

    working on sidebar menu feature

--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -3,6 +3,9 @@
 import { Coffee, EllipsisVertical, LogOut } from "lucide-react";
 import {
   Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -18,13 +21,23 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  SIDEBAR_MENU_LIST,
+  SidebarMenuKey,
+} from "@/constants/sidebar-constant";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export default function AppSidebar() {
   const { isMobile } = useSidebar();
-
+  const pathname = usePathname();
+  const profile = {
+    name: "Fadli Faturzaman",
+    role: "admin",
+    avatar_url: "",
+  };
   return (
-    <Sidebar>
-      {/* 1. Header Cafe */}
+    <Sidebar collapsible="icon">
       <SidebarHeader className="border-b pb-4">
         <SidebarMenu>
           <SidebarMenuItem>
@@ -33,14 +46,42 @@ export default function AppSidebar() {
                 <div className="bg-teal-500 flex h-8 w-8 items-center justify-center rounded-md text-white">
                   <Coffee className="size-4" />
                 </div>
-                <span>WPU Cafe</span>
+                <div className="flex flex-col gap-0.5 leading-none group-data-[collapsible=icon]:hidden">
+                  <span className="truncate">WPU Cafe</span>
+                </div>
               </div>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
 
-      {/* 2. Menu User (Langsung di bawah Header agar tidak turun ke bawah) */}
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent className="flex flex-col gap-2">
+            <SidebarMenu>
+              {SIDEBAR_MENU_LIST[profile.role as SidebarMenuKey]?.map(
+                (item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild tooltip={item.title}>
+                      <a
+                        href={item.url}
+                        className={cn("px-4 py-3 h-auto", {
+                          "bg-teal-500 text-white hover:bg-teal-500 hover:text-white":
+                            pathname === item.url,
+                        })}
+                      >
+                        {item.icon && <item.icon />}
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ),
+              )}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
       <div className="p-2">
         <SidebarMenu>
           <SidebarMenuItem>
@@ -48,7 +89,8 @@ export default function AppSidebar() {
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton
                   size="lg"
-                  className="w-full hover:bg-sidebar-accent"
+                  // className="w-full hover:bg-sidebar-accent"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                   <Avatar className="h-8 w-8 rounded-lg">
                     <AvatarImage src="" />
@@ -96,7 +138,6 @@ export default function AppSidebar() {
 
                 <DropdownMenuSeparator />
 
-                {/* Tombol Logout di bawahnya */}
                 <DropdownMenuItem className="flex items-center gap-2 cursor-pointer text-red-500 focus:bg-red-50 focus:text-red-600">
                   <LogOut className="size-4" />
                   <span>Log out</span>
