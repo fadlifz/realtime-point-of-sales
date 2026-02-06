432453a608e767ba7d796035208f0122c21f8882
fadlifz <fadlifaturzaman@gmail.com>
2026-02-06 11:27:49 +0700

    feat: implement create new user

--- src/constants/auth-constant.ts
+++ src/constants/auth-constant.ts
@@ -1,5 +1,3 @@
-import { email } from "zod";
-
 export const INITIAL_LOGIN_FORM = {
   email: "",
   password: "",
@@ -19,4 +17,24 @@ export const INITIAL_STATE_PROFILE = {
   name: "",
   role: "",
   avatar_url: "",
+};
+
+export const INITIAL_CREATE_USER_FORM = {
+  name: "",
+  role: "",
+  avatar_url: "",
+  email: "",
+  password: "",
+};
+
+export const INITIAL_STATE_CREATE_USER = {
+  status: "idle",
+  errors: {
+    email: [],
+    password: [],
+    name: [],
+    role: [],
+    avatar_url: [],
+    _form: [],
+  },
 };
